e6a98f9aaa9005961b9935701cb696a950662b70
FatulM <madani_amirreza@yahoo.com>
2021-03-27 16:29:19 +0430

    ADDED annotations and base classes

--- sealed_annotations/lib/src/annotations.dart
+++ sealed_annotations/lib/src/annotations.dart
@@ -1,1 +1,48 @@
-class Sealed {}
+import 'package:equatable/equatable.dart';
+
+class Sealed {
+  final SealedEquality equality;
+
+  const Sealed({
+    this.equality = SealedEquality.data,
+  });
+}
+
+enum SealedEquality {
+  data,
+  identity,
+  distinct,
+}
+
+@SealedEquality.data
+abstract class SealedEqualityData extends Equatable {
+  @override
+  List<Object?> get props;
+
+  @override
+  String toString();
+}
+
+@SealedEquality.identity
+abstract class SealedEqualityIdentity {
+  @override
+  bool operator ==(Object other) => identical(this, other);
+
+  @override
+  int get hashCode => identityHashCode(this);
+
+  @override
+  String toString();
+}
+
+@SealedEquality.distinct
+abstract class SealedEqualityDistinct {
+  @override
+  bool operator ==(Object other) => false;
+
+  @override
+  int get hashCode => identityHashCode(this);
+
+  @override
+  String toString();
+}
