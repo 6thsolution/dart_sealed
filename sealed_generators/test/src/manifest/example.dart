841f551f63da7bfd8ff7a9c1bae2670db98e0fc1
FatulM <madani_amirreza@yahoo.com>
2021-03-29 16:03:43 +0430

    fixed example

--- sealed_generators/test/src/manifest/example.dart
+++ sealed_generators/test/src/manifest/example.dart
@@ -1,17 +1,35 @@
 import 'package:sealed_generators/src/manifest/manifest.dart';
 
-final mi1 = ManifestItem(name: 'Sunny', fields: []);
-final mi2f1 = ManifestField(name: 'rain', type: 'int');
-final mi2 = ManifestItem(name: 'Rainy', fields: [mi2f1]);
-final mi3f1 = ManifestField(name: 'velocity', type: 'double');
-final mi3f2 = ManifestField(name: 'angle', type: 'double?');
-final mi3 = ManifestItem(name: 'Windy', fields: [mi3f1, mi3f2]);
-final manifest = Manifest(name: 'Weather', items: [mi1, mi2, mi3]);
+final manifest = Manifest(
+  name: 'Weather',
+  items: [
+    ManifestItem(
+      name: 'Sunny',
+      fields: [],
+    ),
+    ManifestItem(
+      name: 'Rainy',
+      fields: [
+        ManifestField(name: 'rain', type: 'int'),
+      ],
+    ),
+    ManifestItem(
+      name: 'Windy',
+      fields: [
+        ManifestField(name: 'velocity', type: 'double'),
+        ManifestField(name: 'angle', type: 'double?')
+      ],
+    ),
+  ],
+);
 
-final code = '''abstract class _Weather {
+final code = '''
+@Sealed()
+abstract class _Weather {
   void sunny();
 
   void rainy(int rain);
 
   void windy(double velocity, double? angle);
-}''';
+}
+''';
