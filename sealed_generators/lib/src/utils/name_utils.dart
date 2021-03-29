ea163354328ec902e7b11035ff0fab9cb61a74a6
FatulM <madani_amirreza@yahoo.com>
2021-03-29 15:43:07 +0430

    use extension methods instead of static

--- sealed_generators/lib/src/utils/name_utils.dart
+++ sealed_generators/lib/src/utils/name_utils.dart
@@ -1,35 +1,31 @@
-class NameUtils {
-  NameUtils._() {
-    throw AssertionError();
-  }
-
-  static bool startWithUpper(String name) {
-    assert(name.isNotEmpty && name.trim() == name);
-    final first = name.substring(0, 1);
+extension NameUtils on String {
+  bool startWithUpper() {
+    assert(isNotEmpty && trim() == this);
+    final first = substring(0, 1);
     assert(first != '_');
     return first == first.toUpperCase();
   }
 
-  static bool startWithLower(String name) {
-    assert(name.isNotEmpty && name.trim() == name);
-    final first = name.substring(0, 1);
+  bool startWithLower() {
+    assert(isNotEmpty && trim() == this);
+    final first = substring(0, 1);
     assert(first != '_');
     return first == first.toLowerCase();
   }
 
-  static String toUpperStart(String name) {
-    assert(name.isNotEmpty && name.trim() == name);
-    final first = name.substring(0, 1);
+  String toUpperStart() {
+    assert(isNotEmpty && trim() == this);
+    final first = substring(0, 1);
     assert(first != '_');
-    final second = name.substring(1);
+    final second = substring(1);
     return first.toUpperCase() + second;
   }
 
-  static String toLowerStart(String name) {
-    assert(name.isNotEmpty && name.trim() == name);
-    final first = name.substring(0, 1);
+  String toLowerStart() {
+    assert(isNotEmpty && trim() == this);
+    final first = substring(0, 1);
     assert(first != '_');
-    final second = name.substring(1);
+    final second = substring(1);
     return first.toLowerCase() + second;
   }
 }
--- sealed_generators/test/src/utils/name_utils_test.dart
+++ sealed_generators/test/src/utils/name_utils_test.dart
@@ -4,39 +4,39 @@ import 'package:test/test.dart';
 void main() {
   group('class NameUtils', () {
     test('method startWithUpper', () {
-      expect(() => NameUtils.startWithUpper(''), throwsA(anything));
-      expect(() => NameUtils.startWithUpper(' d '), throwsA(anything));
-      expect(() => NameUtils.startWithUpper('_A'), throwsA(anything));
+      expect(() => ''.startWithUpper(), throwsA(anything));
+      expect(() => ' d '.startWithUpper(), throwsA(anything));
+      expect(() => '_A'.startWithUpper(), throwsA(anything));
 
-      expect(NameUtils.startWithUpper('Apple'), equals(true));
-      expect(NameUtils.startWithUpper('apple'), equals(false));
+      expect('Apple'.startWithUpper(), equals(true));
+      expect('apple'.startWithUpper(), equals(false));
     });
 
     test('method startWithLower', () {
-      expect(() => NameUtils.startWithLower(''), throwsA(anything));
-      expect(() => NameUtils.startWithLower(' d '), throwsA(anything));
-      expect(() => NameUtils.startWithLower('_A'), throwsA(anything));
+      expect(() => ''.startWithLower(), throwsA(anything));
+      expect(() => ' d '.startWithLower(), throwsA(anything));
+      expect(() => '_A'.startWithLower(), throwsA(anything));
 
-      expect(NameUtils.startWithLower('Apple'), equals(false));
-      expect(NameUtils.startWithLower('apple'), equals(true));
+      expect('Apple'.startWithLower(), equals(false));
+      expect('apple'.startWithLower(), equals(true));
     });
 
     test('method toUpperStart', () {
-      expect(() => NameUtils.toUpperStart(''), throwsA(anything));
-      expect(() => NameUtils.toUpperStart(' d '), throwsA(anything));
-      expect(() => NameUtils.toUpperStart('_A'), throwsA(anything));
+      expect(() => ''.toUpperStart(), throwsA(anything));
+      expect(() => ' d '.toUpperStart(), throwsA(anything));
+      expect(() => '_A'.toUpperStart(), throwsA(anything));
 
-      expect(NameUtils.toUpperStart('Apple'), equals('Apple'));
-      expect(NameUtils.toUpperStart('apple'), equals('Apple'));
+      expect('Apple'.toUpperStart(), equals('Apple'));
+      expect('apple'.toUpperStart(), equals('Apple'));
     });
 
     test('method toLowerStart', () {
-      expect(() => NameUtils.toLowerStart(''), throwsA(anything));
-      expect(() => NameUtils.toLowerStart(' d '), throwsA(anything));
-      expect(() => NameUtils.toLowerStart('_A'), throwsA(anything));
+      expect(() => ''.toLowerStart(), throwsA(anything));
+      expect(() => ' d '.toLowerStart(), throwsA(anything));
+      expect(() => '_A'.toLowerStart(), throwsA(anything));
 
-      expect(NameUtils.toLowerStart('Apple'), equals('apple'));
-      expect(NameUtils.toLowerStart('apple'), equals('apple'));
+      expect('Apple'.toLowerStart(), equals('apple'));
+      expect('apple'.toLowerStart(), equals('apple'));
     });
   });
 }
