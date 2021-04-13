65a5075fe023d43f7246c0f6570d33f6c3ed9c9f
FatulM <madani_amirreza@yahoo.com>
2021-04-13 14:30:19 +0430

    added basic mapper example

---
+++ sealed_super_enum_mapper_example/bin/weather.dart
@@ -0,0 +1,12 @@
+part 'weather.super.dart';
+
+@Object()
+// ignore: unused_element
+enum _Weather {
+  // ignore: unused_field
+  sunny,
+  // ignore: unused_field
+  rainy,
+  // ignore: unused_field
+  windy,
+}
---
+++ sealed_super_enum_mapper_example/bin/weather.super.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather.dart';
+
+// **************************************************************************
+// Generator: sealed_super_enum_mapper
+// **************************************************************************
+
+// HELLO, THERE!
