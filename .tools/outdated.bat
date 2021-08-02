c8b0567dbc853f9a7289b982d6b650d9c21eea86
FatulM <madani_amirreza@yahoo.com>
2021-08-02 13:03:52 +0430

    ci(all): added outdated tool

---
+++ .tools/outdated.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo HEADER init
+
+pushd sealed_annotations || exit 1
+echo NOTE sealed_annotations
+call dart pub outdated || exit 1
+popd || exit 1
+
+pushd sealed_writer || exit 1
+echo NOTE sealed_writer
+call dart pub outdated || exit 1
+popd || exit 1
+
+pushd sealed_generators || exit 1
+echo NOTE sealed_generators
+call dart pub outdated || exit 1
+popd || exit 1
+
+pushd sealed_example || exit 1
+echo NOTE sealed_example
+call dart pub outdated || exit 1
+popd || exit 1
